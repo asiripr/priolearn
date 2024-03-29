757d6205eaa40570e956ec2599f2ead5cefc71fc
Asiri Pramodaya <asiripramodaya2000@gmail.com>
2024-03-29 17:14:46 +0530

    added tittle, two text fields, and login button on login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,9 +5,84 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: Scaffold(
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            const Text(
+              "Log In",
+              style: TextStyle(color: Colors.black, fontSize:45 
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.blue[100],
+                    hintText: "Email",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.blue)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Color.fromARGB(255, 0, 140, 255))
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.red)
+                    ),
+                  ),
+                ),
+              ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.blue)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Color.fromARGB(255, 0, 140, 255))
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(color: Colors.red)
+                    ),
+                  ),
+                ),
+              ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: SizedBox(
+                  width:double.infinity,
+                  height: 55,
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    child: Text("Log In",style: TextStyle(fontSize: 30, color: Colors.black),),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                  ),
+                ),
+              )
+          ],
+        ),
+      ),
     );
   }
 }
