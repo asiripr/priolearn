ab4c8c169059716f95418c1e437b06954f33263b
Shavindu-Keshan <jkskeshan@std.appsc.sab.ac.lk>
2024-04-02 10:18:16 +0530

    I add saveal page

--- lib/splash.dart
+++ lib/splash.dart
@@ -9,16 +9,18 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
-
- @override
+  @override
   void initState() {
     super.initState();
     _navigateToHome();
   }
-  void _navigateToHome() async{
-    await Future.delayed(const Duration(milliseconds: 2500));
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const Login()));
+
+  void _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 12500));
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const Login()));
   }
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -29,70 +31,68 @@ class _SplashState extends State<Splash> {
         body: Stack(
           children: [
             Positioned(
-              top: -screenHeight*0.3,
-              left: screenWidth*0.05,
-              child: Container(
-                width: 250.0,
-                height: 250.0,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF475FE0).withOpacity(0.5),
-                  shape: BoxShape.circle
-                ),
-              )
-            ),
+                top: -screenHeight * 0.3,
+                left: screenWidth * 0.05,
+                child: Container(
+                  width: 250.0,
+                  height: 250.0,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF475FE0).withOpacity(0.5),
+                      shape: BoxShape.circle),
+                )),
             Positioned(
-              top: -screenHeight*0.08,
-              left: -screenWidth*0.15,
-              child: Container(
-                width: 180.0,
-                height: 180.0,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF5EE689).withOpacity(0.5),
-                  shape: BoxShape.circle
-                ),
-              )
-            ),
+                top: -screenHeight * 0.08,
+                left: -screenWidth * 0.15,
+                child: Container(
+                  width: 180.0,
+                  height: 180.0,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF5EE689).withOpacity(0.5),
+                      shape: BoxShape.circle),
+                )),
             Center(
               child: Padding(
-                padding: EdgeInsets.all(screenHeight*0.15),
-                child: Column( 
-                 children: [
-                  Text(
-                    "PrioLearn", style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold , color: Colors.blue[600],)
-                  ),
-                  SizedBox(
-                      height: screenHeight*0.06,
-                  ),
-                  Positioned(
-                    child: Image.asset("assets/image-1.png",
-                    height: 250,
-                    width: 250
+                padding: EdgeInsets.all(screenHeight * 0.15),
+                child: Column(
+                  children: [
+                    Text("PrioLearn",
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[600],
+                        )),
+                    SizedBox(
+                      height: screenHeight * 0.06,
                     ),
-                  ),
-                  SizedBox(
-                      height: screenHeight*0.01,
-                  ),
-                  const Text("Unlock your academic potential and\n embrace balance with PrioLearn.", textAlign: TextAlign.center,)
-                 ],
-                ),),
+                    Positioned(
+                      child: Image.asset("assets/image-1.png",
+                          height: 250, width: 250),
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.01,
+                    ),
+                    const Text(
+                      "Unlock your academic potential and\n embrace balance with PrioLearn.",
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             ),
             Positioned(
-              top: screenHeight*0.55,
-              left: screenWidth*0.75,
+              top: screenHeight * 0.55,
+              left: screenWidth * 0.75,
               child: Container(
                 width: 400.0,
                 height: 400.0,
                 decoration: BoxDecoration(
-                  color: const Color(0xFF50C0FF).withOpacity(0.5),
-                  shape: BoxShape.circle
-                ),
+                    color: const Color(0xFF50C0FF).withOpacity(0.5),
+                    shape: BoxShape.circle),
               ),
             )
           ],
         ),
       ),
     );
-    
   }
 }
-
