adc022e8b865054a4ff674003e6c48b7f9c7e66f
Asiri Pramodaya <asiripramoday2000@gmail.com>
2024-04-22 18:46:09 +0530

    I pasted 2 images into assets folder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,6 +16,51 @@ class _HomeState extends State<Home> {
         appBar: AppBar(
           title: Center(child: Text("PrioLearn", style: TextStyle(color: Colors.blue,),)),
           ),
+        endDrawer: Drawer(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
+              children: [
+                ListTile(
+                  title: const Text("To Do List"),
+                  onTap: (){},
+                ),
+                ListTile(
+                  title: const Text("Reduce Stress"),
+                  onTap: (){},
+                ),
+                ListTile(
+                  title: const Text("Make a Plan"),
+                  onTap: (){},
+                ),
+                ListTile(
+                  title: const Text("App Settings"),
+                  onTap: (){},
+                ),
+                ListTile(
+                  title: const Text("Invite Friends"),
+                  onTap: (){},
+                ),
+                ListTile(
+                  title: const Text("About App"),
+                  onTap: (){},
+                ),
+                ListTile(
+                  title: const Text("Contact Us"),
+                  onTap: (){},
+                ),
+                ListTile(
+                  title: const Text("Terms and Conditions"),
+                  onTap: (){},
+                ),
+                ListTile(
+                  title: const Text("Settings"),
+                  onTap: (){},
+                ),
+              ],
+            ),
+          ),
+        ),
         ),
     );
   }
