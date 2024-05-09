dc29484868628c44f3b356d9fd2349dfbdc96123
Asiri Pramodaya <asiripramoday2000@gmail.com>
2024-05-09 14:57:16 +0530

    make modifications on main.dart file

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:group_13_priolearn/mindfulness/Mood_check.dart';
+import 'package:group_13_priolearn/mindfulness/mood_check.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
