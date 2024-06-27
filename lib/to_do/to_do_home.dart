5062974443c1f594dd93df206559153245efba5d
Asiri Pramodaya <asiripramoday2000@gmail.com>
2024-06-27 12:23:13 +0530

    created new dart file for to do home

---
+++ lib/to_do/to_do_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ToDoHome extends StatefulWidget {
+  const ToDoHome({super.key});
+
+  @override
+  State<ToDoHome> createState() => _ToDoHomeState();
+}
+
+class _ToDoHomeState extends State<ToDoHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
