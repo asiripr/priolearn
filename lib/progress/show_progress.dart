642712d9a221ad7df5ed91620b220ba16cb69284
asiripr <asiripramodaya2000@gmail.com>
2024-07-27 09:07:06 +0530

    make fixes

--- lib/progress/show_progress.dart
+++ lib/progress/show_progress.dart
@@ -11,7 +11,15 @@ class ShowProgress extends StatefulWidget {
 }
 
 class _ShowProgressState extends State<ShowProgress> {
-  List<double> weeklySummary = [0,0,0,0,0,0,0,];
+  List<double> weeklySummary = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ];
 
   @override
   void initState() {
@@ -19,80 +27,78 @@ class _ShowProgressState extends State<ShowProgress> {
     _fetchWeeklyData();
   }
 
-
   //create a method for fetch weekly data--------
   Future<void> _fetchWeeklyData() async {
-  final DateTime now = DateTime.now();
-  DateTime startOfWeek;
-  DateTime endOfWeek;
+    final DateTime now = DateTime.now();
+    DateTime startOfWeek;
+    DateTime endOfWeek;
 
-  // If today is Sunday (start of a new week), get the previous week's data
-  if (now.weekday == DateTime.sunday) {
-    endOfWeek = now.subtract(Duration(days: 1));
-    startOfWeek = endOfWeek.subtract(Duration(days: 6));
-  } else {
-    endOfWeek = now.subtract(Duration(days: now.weekday));
-    startOfWeek = endOfWeek.subtract(Duration(days: 6));
-  }
-
-  // get the relevant dataset
-  final QuerySnapshot snapshot = await FirebaseFirestore.instance
-      .collection('tasks')
-      .where('type', isEqualTo: 'Academic')
-      .where('date', isGreaterThanOrEqualTo: startOfWeek)
-      .where('date', isLessThanOrEqualTo: endOfWeek)
-      .get();
-
-  final List<QueryDocumentSnapshot> documents = snapshot.docs;
-
-  Map<String, int> tempWeeklyData = {
-    'Sunday': 0,
-    'Monday': 0,
-    'Tuesday': 0,
-    'Wednesday': 0,
-    'Thursday': 0,
-    'Friday': 0,
-    'Saturday': 0,
-  };
-
-  for (var doc in documents) {
-    dynamic dateData = doc['date'];
-    DateTime date;
-
-    if (dateData is Timestamp) {
-      date = dateData.toDate();
-    } else if (dateData is String) {
-      date = DateTime.parse(dateData);
+    // If today is Sunday (start of a new week), get the previous week's data
+    if (now.weekday == DateTime.sunday) {
+      endOfWeek = now.subtract(Duration(days: 1));
+      startOfWeek = endOfWeek.subtract(Duration(days: 6));
     } else {
-      continue;
+      endOfWeek = now.subtract(Duration(days: now.weekday));
+      startOfWeek = endOfWeek.subtract(Duration(days: 6));
     }
 
-    String dayOfWeek = DateFormat('EEEE').format(date);
-    int duration = (doc['duration'] as num).toInt();
-    tempWeeklyData[dayOfWeek] = (tempWeeklyData[dayOfWeek] ?? 0) + duration;
+    // get the relevant dataset
+    final QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('tasks')
+        .where('type', isEqualTo: 'Academic')
+        .where('date', isGreaterThanOrEqualTo: startOfWeek)
+        .where('date', isLessThanOrEqualTo: endOfWeek)
+        .get();
+
+    final List<QueryDocumentSnapshot> documents = snapshot.docs;
+
+    Map<String, int> tempWeeklyData = {
+      'Sunday': 0,
+      'Monday': 0,
+      'Tuesday': 0,
+      'Wednesday': 0,
+      'Thursday': 0,
+      'Friday': 0,
+      'Saturday': 0,
+    };
+
+    for (var doc in documents) {
+      dynamic dateData = doc['date'];
+      DateTime date;
+
+      if (dateData is Timestamp) {
+        date = dateData.toDate();
+      } else if (dateData is String) {
+        date = DateTime.parse(dateData);
+      } else {
+        continue;
+      }
+
+      String dayOfWeek = DateFormat('EEEE').format(date);
+      int duration = (doc['duration'] as num).toInt();
+      tempWeeklyData[dayOfWeek] = (tempWeeklyData[dayOfWeek] ?? 0) + duration;
+    }
+
+    setState(() {
+      weeklySummary = [
+        tempWeeklyData['Sunday']!.toDouble(),
+        tempWeeklyData['Monday']!.toDouble(),
+        tempWeeklyData['Tuesday']!.toDouble(),
+        tempWeeklyData['Wednesday']!.toDouble(),
+        tempWeeklyData['Thursday']!.toDouble(),
+        tempWeeklyData['Friday']!.toDouble(),
+        tempWeeklyData['Saturday']!.toDouble(),
+      ];
+    });
   }
 
-  setState(() {
-    weeklySummary = [
-      tempWeeklyData['Sunday']!.toDouble(),
-      tempWeeklyData['Monday']!.toDouble(),
-      tempWeeklyData['Tuesday']!.toDouble(),
-      tempWeeklyData['Wednesday']!.toDouble(),
-      tempWeeklyData['Thursday']!.toDouble(),
-      tempWeeklyData['Friday']!.toDouble(),
-      tempWeeklyData['Saturday']!.toDouble(),
-    ];
-  });
-}
-
-    Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Progress"),
       ),
-      body:MainBarGraph(
-                weeklySummary: weeklySummary),);
-       // replace by my bar graph
-    
+      body: MainBarGraph(weeklySummary: weeklySummary),
+    );
+    // replace by my bar graph
   }
 }
