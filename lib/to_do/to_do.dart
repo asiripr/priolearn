adf6f851762b37f4d698365a8a4edd5db70035f7
asiripr <asiripramodaya2000@gmail.com>
2024-07-28 11:25:23 +0530

    working for improve todo

---
+++ lib/to_do/to_do.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ToDo extends StatefulWidget {
+  const ToDo({super.key});
+
+  @override
+  State<ToDo> createState() => _ToDoState();
+}
+
+class _ToDoState extends State<ToDo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
