e25bb7f2221691eeb646c6204b072fd9df7f91c6
asiripr <asiripramodaya2000@gmail.com>
2024-08-19 12:58:49 +0530

    created make happy screen

---
+++ lib/mindfulness/make_happy.dart
@@ -0,0 +1,55 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:group_13_priolearn/utils/bottom_app_bar.dart';
+
+class MakeHappyStateful extends StatefulWidget {
+  final List<String> suggestions;
+  const MakeHappyStateful({super.key, required this.suggestions});
+
+  @override
+  _MakeHappyState createState() => _MakeHappyState();
+}
+
+class _MakeHappyState extends State<MakeHappyStateful> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    if (_selectedIndex != index) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final random = Random();
+    final randomSuggestion = widget.suggestions[random.nextInt(widget.suggestions.length)];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Be Happy', style: TextStyle(color: Colors.blue)),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(
+            randomSuggestion,
+            style: TextStyle(fontSize: 16, color: Colors.black),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      bottomNavigationBar: MyBottomNavBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -4,11 +4,11 @@ import 'package:group_13_priolearn/models/model.dart';
 class QuestionModel with ChangeNotifier {
   List<Question> questions = [
     Question("Which hobby do you enjoy most?"),
-    Question("What is your favourite sport?"),
-    Question("Who is your favourite singer?"),
+    Question("What is your favorite sport?"),
+    Question("Who is your favorite singer?"),
     Question("What is your dream travel destination?"),
     Question("Which movie do you enjoy the most?"),
-    Question("What is your favourite dish?"),
+    Question("What is your favorite dish?"),
   ];
 
   int currentPage = 0;
@@ -22,6 +22,25 @@ class QuestionModel with ChangeNotifier {
     if (currentPage < questions.length - 1) {
       currentPage++;
       notifyListeners();
+    }
+  }
+
+  String _generateSuggestion(String question, String answer){
+    switch (question) {
+      case "Which hobby do you enjoy most?":
+        return "How about dedicating some time to $answer this weekend?";
+      case "What is your favorite sport?":
+        return "Why not catch a game of $answer soon?";
+      case "Who is your favorite singer?":
+        return "How about putting on some music by $answer? It might help you relax.";
+      case "What is your dream travel destination?":
+        return "Start planning your trip to $answer. It's always good to have something to look forward to!";
+      case "Which movie do you enjoy the most?":
+        return "Rewatch $answer, it's always a good time!";
+      case "What is your favorite dish?":
+        return "Maybe you can treat yourself to $answer sometime soon!";
+      default:
+        return " ";
     }
   }
 }
